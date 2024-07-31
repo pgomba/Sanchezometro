14e1982061f606f6ae633e1aabd988e76025f18e
Pablo Gomez Barreiro <p.gomezbarreiro@kew.org>
2024-07-31 14:15:38 +0100

    initial test

---
+++ sanchezometro.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(rvest)
+
+date<-Sys.Date()
+
+okdiario_text<-read_html("https://okdiario.com/")%>%
+  html_nodes(".segmento-link")%>%
+  html_text2()
+
+elpais_text<-read_html("https://elpais.com/")%>%
+  html_nodes(".c_t a")%>%
+  html_text2()
+
+elmundo_text<-  read_html("https://www.elmundo.es/")%>%
+  html_nodes(".ue-c-cover-content__link")%>%
+  html_text2()
+
+minutos_text<- read_html("https://www.20minutos.es/")%>%
+  html_nodes(".media-content a")%>%
+  html_text2()
+
+abc_text<-read_html("https://www.abc.es/")%>%
+  html_nodes("a")%>%
+  html_text2()
+
+publico_text <-read_html("https://www.publico.es/")%>%
+  html_nodes(".title a")%>%
+  html_text2()
+
+okdiaro<-length(grep("Sánchez", okdiario_text))
+elpais<-(grep("Sánchez", elpais_text))
+elmundo<-(grep("Sánchez", elmundo_text))
+minutos<-(grep("Sánchez", minutos_text))
+abc<-(grep("Sánchez", abc_text))
+publico<-(grep("Sánchez", publico_text))
+
+temp_df<-data.frame(date,okdiaro,elpais,elmundo,minutos,abc,publico)
+
+url<-read.csv("https://raw.githubusercontent.com/pgomba/sanchezometro/main/sanchez_data/sanchez_counter.csv")
+
+sanchez_values<-bind_rows(original_data,mdpi_rej_rate_track)
+
+
+write.csv(sanchez_values,row.names = FALSE,paste0("sanchez_data/sanchez_counter.csv"))
